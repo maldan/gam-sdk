0ca6015b0bec36c07309b21a20a5c170af324e3a
Roman <blackwanted@yandex.ru>
2021-09-12 11:44:26 +0300

    Start

---
+++ vue/component/TextArea.vue
@@ -0,0 +1,52 @@
+<template>
+  <div :class="$style.textarea">
+    <textarea :placeholder="placeholder" :value="modelValue" @input="change"></textarea>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    placeholder: String,
+    modelValue: String,
+  },
+  async mounted() {},
+  methods: {
+    change(e: any) {
+      this.$emit('update:modelValue', e.target.value);
+    },
+  },
+  data: () => {
+    return {};
+  },
+});
+</script>
+
+<style module lang="scss">
+.textarea {
+  display: flex;
+  align-items: center;
+  flex: 1;
+  box-sizing: border-box;
+
+  textarea {
+    box-sizing: border-box;
+    background: #3d3d3d;
+    color: #9d9d9d;
+    border: 0;
+    padding: 10px;
+    flex: 1;
+    outline: none;
+    font-size: 16px;
+    border: 1px solid transparent;
+
+    min-width: 0;
+
+    &:focus {
+      border: 1px solid #888888;
+    }
+  }
+}
+</style>
