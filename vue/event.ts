fcbc836190e27cf57664a1f67f5ddbf7017ef392
Roman <blackwanted@yandex.ru>
2021-09-19 11:53:58 +0300

    Fix -- Added vue events

---
+++ vue/event.ts
@@ -0,0 +1,22 @@
+export function init(app: any) {
+  app.directive('doubleclick', {
+    created(el: any, binding: any, vnode: any, prevVnode: any) {
+      let clicks = 0;
+      let timer = null as any;
+
+      el.addEventListener('click', (e: any) => {
+        clicks += 1;
+
+        if (clicks === 1) {
+          timer = setTimeout(() => {
+            clicks = 0;
+          }, 300);
+        } else {
+          clearTimeout(timer);
+          clicks = 0;
+          console.log('sas');
+        }
+      });
+    },
+  });
+}
