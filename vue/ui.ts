4fd3bd7edbc67eafa797777911e4fcedee54a173
Roman <blackwanted@yandex.ru>
2021-11-06 05:06:25 +0300

    Sas

--- vue/ui.ts
+++ vue/ui.ts
@@ -1,30 +1,30 @@
 export default {
   install: (app: any, options: any) => {
-    // Register components
-    const components = require.context('./component/', true, /^.*\.vue$/).keys();
-    for (let i = 0; i < components.length; i++) {
-      const name = components[i]
+    // Register ui components
+    const ui = require.context('./component/', true, /^.*\.vue$/).keys();
+    for (let i = 0; i < ui.length; i++) {
+      const name = ui[i]
         .replace('./', '')
         .replace('.vue', '')
         .replace(/\//g, '-')
         .replace(/\./g, '-');
 
-      const sas = require('./component/' + components[i].replace('./', ''));
+      const sas = require('./component/' + ui[i].replace('./', ''));
       app.component(name, sas.default);
     }
 
-    // Register modals
-    const modals = require.context('@/component/modal/', true, /^.*\.vue$/).keys();
-    for (let i = 0; i < modals.length; i++) {
-      const name = modals[i]
+    // Register components globally
+    const components = require.context('@/component/', true, /^.*\.vue$/).keys();
+    for (let i = 0; i < components.length; i++) {
+      const name = components[i]
         .replace('./', '')
         .replace('.vue', '')
         .replace(/\//g, '-')
         .replace(/\./g, '-')
         .toLowerCase();
 
-      const sas = require('@/component/modal/' + modals[i].replace('./', ''));
-      app.component(`modal-${name}`, sas.default);
+      const sas = require('@/component/' + components[i].replace('./', ''));
+      app.component(`${name}`, sas.default);
     }
   },
 };
